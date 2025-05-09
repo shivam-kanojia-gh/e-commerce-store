b3cd146269219358a3b1b640b0c3cc3c6b41673c
Shivam Kanojia <shivamkanojia.works@gmail.com>
2025-05-10 03:38:05 +0530

    chore: establish MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import authRouter from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,4 +11,5 @@ app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
+  connectDB();
 });
